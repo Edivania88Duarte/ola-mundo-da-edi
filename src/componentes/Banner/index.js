957bb2b99d3029cb639b8d4fb344086ab9437c9f
Edivania Duarte <120994730+Edivania88Duarte@users.noreply.github.com>
2025-04-19 22:09:29 -0300

    fix: alteraçao no fetch para rodar back de forma dinâmica

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,41 +1,38 @@
-import styles from './Banner.module.css';
-import circuloColorido from 'assets/circulo_colorido.png'
-import minhaFoto from 'assets/minha_foto.jpg'
-
+import styles from "./Banner.module.css";
+import circuloColorido from "assets/circulo_colorido.png";
+import minhaFoto from "assets/minha_foto.jpg";
 
 export default function Bannner() {
-    return(
-        <div className={styles.banner}>
-            <div>
-                <h1 className={styles.titulo}>
-                    Olá, Mundo!
-                </h1>   
+  return (
+    <div className={styles.banner}>
+      <div>
+        <h1 className={styles.titulo}>Olá, Mundo!</h1>
 
-                <p className={styles.paragrafo}>
-                Seja bem-vindo(a) ao meu cantinho! <br></br>
-Eu sou a Edivania Duarte, mas pode me chamar de Edi :) <br></br>
-Sou desenvolvedora Full Stack em formação e estagiária de desenvolvimento, sempre sedenta por novos aprendizados e apaixonada por criar conexões.
-Aqui compartilho o conhecimento que estou adquirindo ao longo da minha jornada. Espero que algo que eu compartilhe aqui possa ser útil e inspirador para você!
-                </p>
-            </div>
+        <p className={styles.paragrafo}>
+          Seja bem-vindo(a) ao meu cantinho! <br></br>
+          Eu sou a Edivania Duarte, mas pode me chamar de Edi :) <br></br>
+          Sou desenvolvedora Full Stack em formação e estagiária de
+          desenvolvimento, sempre sedenta por novos aprendizados e apaixonada
+          por criar conexões. Aqui compartilho o conhecimento que estou
+          adquirindo ao longo da minha jornada. Espero que algo que eu
+          compartilhe aqui possa ser útil e inspirador para você!
+        </p>
+      </div>
 
-            <div className={styles.imagens}>
-                <img 
-                    className={styles.circuloColorido}
-                    src={circuloColorido}
-                    alt="Foto da circulo colorido"
-                    aria-hidden={true}
-                />
+      <div className={styles.imagens}>
+        <img
+          className={styles.circuloColorido}
+          src={circuloColorido}
+          alt="Foto da circulo colorido"
+          aria-hidden={true}
+        />
 
-                <img
-                    className={styles.minhaFoto}
-                    src={minhaFoto}
-                    alt="Foto da Edi sorrindo"
-                
-                />
-
-            </div>
-       
-        </div>
-    )
+        <img
+          className={styles.minhaFoto}
+          src={minhaFoto}
+          alt="Foto da Edi sorrindo"
+        />
+      </div>
+    </div>
+  );
 }
--- src/paginas/Contato/index.js
+++ src/paginas/Contato/index.js
@@ -19,7 +19,7 @@ export default function Contato() {
         };
     
         try {
-            const response = await fetch('http://localhost:3001/send-email', {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/send-email`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
